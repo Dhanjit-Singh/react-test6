bb5a6c1ec9ec555529ee8385ce4a47774867ecc3
Dhanjit-Singh <dhanjit.nrit@gmail.com>
2025-02-16 16:55:27 +0530

    updated

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -2,78 +2,76 @@ import React, { useState, useEffect } from "react";
 import "../Css/Todos.css";
 import todoImg from "../assets/images/todo.svg";
 import { FaPlus } from "react-icons/fa6";
+import { IoTrashBinOutline } from "react-icons/io5";
+import { FiEdit } from "react-icons/fi";
 
-
-
-// get the localStorage data back
-// const getLocalData = () => {
-//     const lists = localStorage.getItem("mytodolist");
-
-//     if (lists) {
-//         return JSON.parse(lists);
-//     } else {
-//         return [];
-//     }
-// };
 
 function Todos() {
     const [inputdata, setInputData] = useState("");
-    const [items, setItems] = useState([]);
-    const [isEditItem, setIsEditItem] = useState("");
-    const [toggleButton, setToggleButton] = useState(false);
+    const [items, setItems] = useState(() => {
+        const savedItems = localStorage.getItem("todoItems");
+        return savedItems ? JSON.parse(savedItems) : []; // Ensuring an array is returned
+    });
+    const [editId, setEditId] = useState(null);
+    // localStorage.removeItem("todoItems");
+
+    useEffect(() => {
+        localStorage.setItem("todoItems", JSON.stringify(items));
+    }, [items]);
 
     // add the items fucnction
     const addItem = () => {
         if (!inputdata) {
             alert("plz fill the data");
-        } else if (inputdata) {
-            setItems(
-                [...items, { name: inputdata }]
-            );
-            // setInputData("");
+            return;
+        } 
+        
+        const editData = items.find((curElem) => curElem.name.toLowerCase() === inputdata.toLowerCase());
+        if(editData) {
+            console.log("This item already exists!");
+            setInputData("");
+            return;
+        }
 
-            //     setInputData("");
-            //     setIsEditItem(null);
-            //     setToggleButton(false);
+        if (editId) {
+            const updatedItems = items.map((item) =>
+                item.id === editId ? { ...item, name: inputdata } : item
+            );
+            setItems(updatedItems);
+            setEditId(null);
+        } else {
+            const newItem = { id: new Date().getTime(), name: inputdata };
+            setItems([...items, newItem]);
+
+        }
+
+            // if(editData) {
+            //     console.log("This item already exists!");
             // } else {
-            //     const myNewInputData = {
-            //         id: new Date().getTime().toString(),
+            //     const newItem = {
+            //         id: new Date().getTime(), // Generate a unique ID
             //         name: inputdata,
             //     };
-            //     setItems([...items, myNewInputData]);
-            //     setInputData("");
-        }
+            //     setItems([...items, newItem]);
+            // }
+            // }
+        setInputData("");
     };
-    console.log('inputdata=====>>>', inputdata);
-    console.log('item=====>>>', items);
 
     //edit the items
-    // const editItem = (index) => {
-    //     const item_todo_edited = items.find((curElem) => {
-    //         return curElem.id === index;
-    //     });
-    //     setInputData(item_todo_edited.name);
-    //     setIsEditItem(index);
-    //     setToggleButton(true);
-    // };
+    const editItem = (id) => {
+        const editAble = items.find((curElem) => curElem.id === id);
+        if(editAble) {
+            setInputData(editAble.name);
+            setEditId(id);
+        }
+    };
 
     // how to delete items section
-    // const deleteItem = (index) => {
-    //     const updatedItems = items.filter((curElem) => {
-    //         return curElem.id !== index;
-    //     });
-    //     setItems(updatedItems);
-    // };
-
-    // remove all the elements
-    // const removeAll = () => {
-    //     setItems([]);
-    // };
-
-    // adding localStorage
-    // useEffect(() => {
-    //     localStorage.setItem("mytodolist", JSON.stringify(items));
-    // }, [items]);
+    const deleteItem = (id) => {
+        const updatedItems = items.filter((curElem) => curElem.id !== id);
+        setItems(updatedItems);
+    };
 
     return (
         <>
@@ -95,11 +93,18 @@ function Todos() {
                     </div>
                     {/* show our items  */}
                     <div className="showItems">
+                        <h4 style={{
+                            color: "white",
+                            fontSize: "24px",
+                            padding: "0px",
+                            margin: "5px"
+                        }}>Your Todo List</h4>
                         {items.map((item, index) => (
                             <div className="eachItem" key={index}>
                                 <h3>{item.name}</h3>
                                 <div className="todo-btn">
-
+                                    <FiEdit onClick={() => editItem(item.id)} />
+                                    <IoTrashBinOutline onClick={() => deleteItem(item.id)} />
                                 </div>
                             </div>
                         ))}
